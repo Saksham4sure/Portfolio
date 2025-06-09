0eaeeea2bae3eed602a04cbd522a8df61d406b12
Saksham Shrestha <saksham@Sakshams-MacBook-Pro.local>
2025-06-09 21:24:14 +0545

    scroll

--- src/main.js
+++ src/main.js
@@ -1,53 +1,164 @@
 import Lenis from 'lenis'
 import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const lenis = new Lenis({
-    autoRaf: true
-  });
+  autoRaf: true
+});
 
-  // gsap.from(".prename", {
-  //   y:500,
-  //   stagger: 0.1,
-  //   duration: 1,
-  //   ease: "power4.out",
-  //   scale: 0.2,
-  //   filter: "blur(10px)",
-  // });
-
-
-
-  //  gsap.to(".prename", {
-  //   x: -500,
-  //   delay: 1.5,
-  //   stagger: 0.1,
-  //   duration: 1,
-  // });
-
-  // gsap.from(".bgbox", {
-  //   x: -2000,
-  //   duration: 1,
-  //   delay: 1.3,
-  // });  
-
-  // gsap.to(".preloader", {
-  //   opacity: 0,
-  //   delay: 2.3,
-  // });
+// gsap.from(".prename", {
+//   y:500,
+//   stagger: 0.1,
+//   duration: 1,
+//   ease: "power4.out",
+//   scale: 0.2,
+//   filter: "blur(10px)",
+// });
 
 
 
-  
-  window.addEventListener('mousemove',  function (dets){
-      gsap.to("#cursor",{
-        left:dets.clientX,
-        top:dets.clientY,
-        
-      })
-  } )
+//  gsap.to(".prename", {
+//   x: -1500,
+//   delay: 1,
+//   stagger: 0.1,
+//   duration: 0.8,
+//   ease: "power4.in",
+
+// });
+
+
+// gsap.to(".preloader", {
+//   x: -1500,
+//   delay: 1.4,
+//   duration: 1,
+//   ease: "power4.in",
+
+// });
+
+
+
+
+gsap.from(".skill", {
+  scrollTrigger: {
+    trigger: ".skill",    
+    start: "top 80%",    
+    end: "bottom 30%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  x: -100,
+  opacity: 0,
+  stagger: 0.1,
+});
+
+gsap.from(".project", {
+  scrollTrigger: {
+    trigger: ".project",    
+    start: "top 90%",    
+    end: "bottom 80%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  y: 200,
+  opacity: 0,
+  stagger: 0.1,
+});
+
+gsap.from(".tags", {
+  scrollTrigger: {
+    trigger: ".project",    
+    start: "top 80%",    
+    end: "bottom 30%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  x: 50,
+  opacity: 0,
+  stagger: 0.1,
+});
+
+
+gsap.from(".project h1", {
+  scrollTrigger: {
+    trigger: ".project",    
+    start: "top 90%",    
+    end: "bottom 30%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  x: -100,
+  opacity: 0,
+  stagger: 0.1,
+});
+
+gsap.from(".project img", {
+  scrollTrigger: {
+    trigger: ".project",    
+    start: "top 90%",    
+    end: "bottom 60%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  scale: 0,
+  y: 100,
+  opacity: 0,
+  stagger: 0.2,
+});
+
+gsap.from(".discri", {
+  scrollTrigger: {
+    trigger: ".project",    
+    start: "top 90%",    
+    end: "bottom 60%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  y: 100,
+  opacity: 0,
+  stagger: 0.2,
+});
+
+gsap.from(".project i", {
+  scrollTrigger: {
+    trigger: ".project",    
+    start: "top 90%",    
+    end: "bottom 60%",
+    scrub: true,    
+    toggleActions: "play none none reverse", 
+    markers: false         
+  },
+  scale: 0,
+  y: 100,
+  opacity: 0,
+  stagger: 0.2,
+});
+
+
+
+
+
+
+window.addEventListener('mousemove', function (dets) {
+  gsap.to("#cursor", {
+    left: dets.clientX,
+    top: dets.clientY,
+
+  })
+})
+
 
 gsap.registerPlugin(CSSRulePlugin);
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let activeItemIndicator = CSSRulePlugin.getRule(".menu-item p#active::after");
   const toggleButton = document.querySelector(".burger")
   let isOpen = false;
@@ -71,7 +182,7 @@ document.addEventListener("DOMContentLoaded", function() {
     stagger: 0.1,
     ease: "power4.out "
   }, "-=1");
-  
+
   timeline.to(activeItemIndicator, {
     width: "100%",
     duration: 1,
@@ -90,7 +201,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }, "<");
 
 
-  toggleButton.addEventListener("click", function() {
+  toggleButton.addEventListener("click", function () {
     if (isOpen) {
       timeline.reverse();
     }
@@ -110,6 +221,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 })
+
+const pimg = document.querySelector(".project img");
+
+document.querySelectorAll('.project').forEach(project =>{
+  project.onclick = () =>{
+
+  }
+})
+
+
+
 
 
 
